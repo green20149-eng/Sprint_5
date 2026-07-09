c94fdee9e77cab19d3a1ff75c60560571932c102
Alexey <green20149@gmail.com>
2026-07-09 13:40:26 +0700

    Create conftest.py

---
+++ conftest.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+import pytest
+
+@pytest.fixture()
+def driver():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("url")
+    yield driver
+    driver.quit()
